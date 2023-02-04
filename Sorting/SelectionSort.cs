822e8c94726c05cb5e143eae5be4cd273f7bca5a
Cubigator <sergeychuprin2006@gmail.com>
2023-02-04 12:54:15 +0700

    Added sort selection

---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpAlgorithms.Sorting
+{
+    public static class SelectionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minimum = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[minimum] > arr[j])
+                    {
+                        minimum = j;
+                    }
+                }
+                (arr[minimum], arr[i]) = (arr[i], arr[minimum]);
+            }
+        }
+    }
+}
